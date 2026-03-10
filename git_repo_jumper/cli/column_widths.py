187bbd035e7bd248871ad164c87d6c5efd590a6a
Corvin Gröning <114770097+cgroening@users.noreply.github.com>
2026-03-10 10:18:38 +0100

    Create new class ColumnWidth (column_widths.py) to replace contents of SelectCommand._adjust_column_widths()

---
+++ git_repo_jumper/cli/column_widths.py
@@ -0,0 +1,13 @@
+class ColumnWidths:
+    _column_widths: dict[str, int]
+    _priority_order: list[str | list[str]]
+
+
+    def _calculate_max_column_widths(self):
+        pass
+
+    def _allocate_extra_space(self):
+        pass
+
+    def _shrink_columns(self):
+        pass
